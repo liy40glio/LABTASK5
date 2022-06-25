d899263c73a6c2684b358797340b6ce74c56f6d5
USER <USER@LAPTOP-6URVNURG>
2022-06-25 12:50:25 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblname = New System.Windows.Forms.Label()
+        Me.label = New System.Windows.Forms.Label()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.txtIC = New System.Windows.Forms.TextBox()
+        Me.txtUname = New System.Windows.Forms.TextBox()
+        Me.txtLname = New System.Windows.Forms.TextBox()
+        Me.txtFname = New System.Windows.Forms.TextBox()
+        Me.ListBox = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblname
+        '
+        Me.lblname.AutoSize = True
+        Me.lblname.Font = New System.Drawing.Font("Arial Rounded MT Bold", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblname.Location = New System.Drawing.Point(275, 626)
+        Me.lblname.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblname.Name = "lblname"
+        Me.lblname.Size = New System.Drawing.Size(63, 18)
+        Me.lblname.TabIndex = 32
+        Me.lblname.Text = "Name :"
+        '
+        'label
+        '
+        Me.label.AutoSize = True
+        Me.label.Font = New System.Drawing.Font("Arial Rounded MT Bold", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label.ForeColor = System.Drawing.Color.Black
+        Me.label.Location = New System.Drawing.Point(755, 274)
+        Me.label.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label.Name = "label"
+        Me.label.Size = New System.Drawing.Size(208, 18)
+        Me.label.TabIndex = 31
+        Me.label.Text = "Without Dash (-), 12 Digit "
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.Tan
+        Me.btnReset.Font = New System.Drawing.Font("Arial Rounded MT Bold", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.ForeColor = System.Drawing.Color.White
+        Me.btnReset.Location = New System.Drawing.Point(903, 134)
+        Me.btnReset.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(112, 51)
+        Me.btnReset.TabIndex = 30
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.Color.BurlyWood
+        Me.btnAdd.Font = New System.Drawing.Font("Arial Rounded MT Bold", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.ForeColor = System.Drawing.Color.White
+        Me.btnAdd.Location = New System.Drawing.Point(903, 56)
+        Me.btnAdd.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(112, 51)
+        Me.btnAdd.TabIndex = 29
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'txtIC
+        '
+        Me.txtIC.Location = New System.Drawing.Point(409, 258)
+        Me.txtIC.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtIC.Name = "txtIC"
+        Me.txtIC.Size = New System.Drawing.Size(309, 26)
+        Me.txtIC.TabIndex = 28
+        '
+        'txtUname
+        '
+        Me.txtUname.Location = New System.Drawing.Point(409, 192)
+        Me.txtUname.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtUname.Name = "txtUname"
+        Me.txtUname.Size = New System.Drawing.Size(309, 26)
+        Me.txtUname.TabIndex = 27
+        '
+        'txtLname
+        '
+        Me.txtLname.Location = New System.Drawing.Point(409, 134)
+        Me.txtLname.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtLname.Name = "txtLname"
+        Me.txtLname.Size = New System.Drawing.Size(309, 26)
+        Me.txtLname.TabIndex = 26
+        '
+        'txtFname
+        '
+        Me.txtFname.Location = New System.Drawing.Point(409, 66)
+        Me.txtFname.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtFname.Name = "txtFname"
+        Me.txtFname.Size = New System.Drawing.Size(309, 26)
+        Me.txtFname.TabIndex = 25
+        '
+        'ListBox
+        '
+        Me.ListBox.FormattingEnabled = True
+        Me.ListBox.ItemHeight = 20
+        Me.ListBox.Location = New System.Drawing.Point(279, 384)
+        Me.ListBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ListBox.Name = "ListBox"
+        Me.ListBox.Size = New System.Drawing.Size(735, 204)
+        Me.ListBox.TabIndex = 24
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(275, 336)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(113, 23)
+        Me.Label5.TabIndex = 23
+        Me.Label5.Text = "Name List:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(275, 261)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(126, 23)
+        Me.Label4.TabIndex = 22
+        Me.Label4.Text = "IC Number :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(275, 192)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(122, 23)
+        Me.Label3.TabIndex = 21
+        Me.Label3.Text = "Username :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(275, 125)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(125, 23)
+        Me.Label2.TabIndex = 20
+        Me.Label2.Text = "Last Name :"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(270, 69)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(127, 23)
+        Me.Label1.TabIndex = 19
+        Me.Label1.Text = "First Name :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.RosyBrown
+        Me.ClientSize = New System.Drawing.Size(1290, 756)
+        Me.Controls.Add(Me.lblname)
+        Me.Controls.Add(Me.label)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtIC)
+        Me.Controls.Add(Me.txtUname)
+        Me.Controls.Add(Me.txtLname)
+        Me.Controls.Add(Me.txtFname)
+        Me.Controls.Add(Me.ListBox)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblname As Label
+    Friend WithEvents label As Label
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents txtIC As TextBox
+    Friend WithEvents txtUname As TextBox
+    Friend WithEvents txtLname As TextBox
+    Friend WithEvents txtFname As TextBox
+    Friend WithEvents ListBox As ListBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Try
+            If IsValidData() Then
+                Dim Fname As String = Convert.ToString(txtFname.Text)
+                Dim Lname As String = Convert.ToString(txtLname.Text)
+                Dim User As String = Convert.ToString(txtUname.Text)
+                Dim IC As String = Convert.ToString(txtIC.Text)
+                ListBox.Items.Add(String.Format("  " & Fname & "  " & Lname & "  " & User & "  " & IC & "  " & Fname & Lname & "@vb.net"))
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message & vbCrLf & vbCrLf & ex.GetType.ToString & vbCrLf & vbCrLf & ex.StackTrace, "Exception")
+        End Try
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        txtFname.Text = ""
+        txtLname.Text = ""
+        txtUname.Text = ""
+        txtIC.Text = ""
+        ListBox.Items.Clear()
+        txtFname.Focus()
+    End Sub
+    Private Function IsValidData() As Boolean
+        Return _
+        IsPresent(txtFname, "First Name") AndAlso
+        IsPresent(txtLname, "Last Name") AndAlso
+        IsPresent(txtUname, "Username") AndAlso
+        IsPresent(txtIC, "IC Number")
+    End Function
+
+    Private Function IsPresent(ByVal textbox As TextBox, ByVal name As String) As Boolean
+        If textbox.Text = "" Then
+            MessageBox.Show(name & " is required", "Entry Error")
+            textbox.Select()
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Const Name As String = "NURAWLIYA NASUHA BT AHMAD ZUBIR (10DDT20F1051) DDT5A"
+        Dim id As String = lblname.Text
+        Dim text As String
+        text = id + Name
+        lblname.Text = text
+    End Sub
+
+    Private Sub txtIC_TextChanged(sender As Object, e As EventArgs) Handles txtIC.TextChanged
+        If txtIC.TextLength > 12 Then
+            MessageBox.Show("you must inserted 12 digit only")
+        End If
+    End Sub
+
+    Private Sub txtIC_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtIC.KeyPress
+
+        If e.KeyChar <> ChrW(8) AndAlso Not (e.KeyChar >= "0"c AndAlso e.KeyChar <= "9"c) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtICnumber_KeyPress_Click(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtIC.KeyPress
+        If e.KeyChar <> ChrW(8) AndAlso Not (e.KeyChar >= "0"c AndAlso e.KeyChar <= "9"c) Then
+            e.Handled = True
+        End If
+    End Sub
+
+End Class
